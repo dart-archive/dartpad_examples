3d9b238944ee00b615faff26db93d7d8f64137c5
Kevin Moore <kevmoo@google.com>
2018-10-17 16:08:30 -0700

    new_mixin_syntax: update example and tests

--- example/dart_2_1/new_mixin_syntax.dart
+++ example/dart_2_1/new_mixin_syntax.dart
@@ -2,13 +2,102 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// TODO(kevmoo): add documentation
 mixin AnswerMixin {
-  int get theAnswer => 42;
+  int get answer => 42;
+
+  @override
+  String toString() => '[ $runtimeType ]';
 }
 
-class MyClass = Object with AnswerMixin;
+class Answer with AnswerMixin {}
 
-main() {
-  print(MyClass().theAnswer);
+void main() {
+  print('''
+*
+* Simple usage of `mixin`
+*
+''');
+  printAnswer(Answer(), 'Use `with` to include a mixin');
+
+  print('''
+*
+* More advanced usage of `mixin`
+*
+''');
+  printAnswer(
+      LogAnswer(),
+      'Include many mixins by separating with commas. '
+      '`$LoggingAnswerMixin` prints every time `answer` is accessed.');
+
+  print('''
+*
+* The order in which mixins are included matters.
+*
+''');
+
+  printAnswer(LogVerifyAnswer(), 'In this case, log then verify.');
+  printAnswer(VerifyLogAnswer(), 'In this case, verify then log.');
+
+  print('''
+*
+* You can extend classes that include mixins, too.
+*
+''');
+
+  printAnswer(DeltaLogVerifyAnswer(), 'Verify will fail.');
+  printAnswer(DeltaLogVerifyAnswer(1), 'Verify will succeed.');
+}
+
+void printAnswer(AnswerMixin obj, String description) {
+  print(obj);
+  print('- $description');
+  print('answer: ${obj.answer}');
+  print('');
+}
+
+/// [LoggingAnswerMixin] can only be used as a mixin when the superclass
+/// (or one of the mixins that comes before it in a "with" clause) implements
+/// [AnswerMixin].
+mixin LoggingAnswerMixin on AnswerMixin {
+  @override
+  int get answer {
+    var value = super.answer;
+    print('  LOG: `answer` property was accessed');
+    return value;
+  }
+}
+
+class LogAnswer with AnswerMixin, LoggingAnswerMixin {}
+
+mixin VerifyingAnswerMixin on AnswerMixin {
+  @override
+  int get answer {
+    var value = super.answer;
+    if (value == 42) {
+      print('  VERIFY: Invalid Result!');
+    } else {
+      print('  VERIFY: valid result');
+    }
+    return value;
+  }
+}
+
+class LogVerifyAnswer
+    with AnswerMixin, LoggingAnswerMixin, VerifyingAnswerMixin {}
+
+class VerifyLogAnswer
+    with AnswerMixin, VerifyingAnswerMixin, LoggingAnswerMixin {}
+
+abstract class DeltaAnswer with AnswerMixin {
+  final int delta;
+
+  DeltaAnswer(this.delta);
+
+  @override
+  int get answer => super.answer + delta;
+}
+
+class DeltaLogVerifyAnswer extends DeltaAnswer
+    with LoggingAnswerMixin, VerifyingAnswerMixin {
+  DeltaLogVerifyAnswer([int delta = 0]) : super(delta);
 }
--- test/dart_2_1/new_mixin_syntax_test.dart
+++ test/dart_2_1/new_mixin_syntax_test.dart
@@ -9,7 +9,55 @@ import '../../example/dart_2_1/new_mixin_syntax.dart' as example;
 void main() {
   test('output', () {
     expect(example.main, prints(r'''
-42
+*
+* Simple usage of `mixin`
+*
+
+[ Answer ]
+- Use `with` to include a mixin
+answer: 42
+
+*
+* More advanced usage of `mixin`
+*
+
+[ LogAnswer ]
+- Include many mixins by separating with commas. `LoggingAnswerMixin` prints every time `answer` is accessed.
+  LOG: `answer` property was accessed
+answer: 42
+
+*
+* The order in which mixins are included matters.
+*
+
+[ LogVerifyAnswer ]
+- In this case, log then verify.
+  LOG: `answer` property was accessed
+  VERIFY: Invalid Result!
+answer: 42
+
+[ VerifyLogAnswer ]
+- In this case, verify then log.
+  VERIFY: Invalid Result!
+  LOG: `answer` property was accessed
+answer: 42
+
+*
+* You can extend classes that include mixins, too.
+*
+
+[ DeltaLogVerifyAnswer ]
+- Verify will fail.
+  LOG: `answer` property was accessed
+  VERIFY: Invalid Result!
+answer: 42
+
+[ DeltaLogVerifyAnswer ]
+- Verify will succeed.
+  LOG: `answer` property was accessed
+  VERIFY: valid result
+answer: 43
+
 '''));
   });
 }
